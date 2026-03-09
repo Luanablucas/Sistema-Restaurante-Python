a9cf97217233f039b75b1482ccd548b9ce6e3701
luanablucas <luanabahialucas2004@gmail.com>
2026-03-09 20:02:53 -0300

    Prática if else

---
+++ praticaif_else.py
@@ -0,0 +1,23 @@
+""" vendas_maca = int(input("Digite a quantidade de maçãs vendidas: "))
+vendas_banana = int(input("Digite a quantidade de bananas vendidas: "))
+
+if vendas_maca > vendas_banana:
+    print("As vendas de maçãs foram maiores do que as vendas de bananas.")
+elif vendas_banana > vendas_maca:
+    print("As vendas de bananas foram maiores do que as vendas de maçãs.")
+else:
+    print("As vendas de maçãs e bananas foram iguais.") """
+
+""" atividade_a = int(input("Informe os dias para a atividade A: "))
+atividade_b = int(input("Informe os dias para a atividade B: "))
+atividade_c = int(input("Informe os dias para a atividade C: "))    
+
+if atividade_a < 0 or atividade_b < 0 or atividade_c < 0:
+    print("Valor inválido! Digite um número positivo.") """
+
+""" temp = float(input("Digite a temperatura atual: "))
+
+if temp > 25:
+    print("Temperatura acima do esperado")
+else:
+    print("Temperatura dentro do esperado") """
